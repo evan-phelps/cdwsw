7663e2aadbf14ebf59191f51b41c08e5b71b281e
Evan Phelps <evan.phelps@gmail.com>
2016-05-28 00:43:11 -0400

    HSSC_ETL user creation with privileges.

---
+++ scripts/sql/user_hssc_etl_create.sql
@@ -0,0 +1,25 @@
+/*
+ * After creating HSSC_ETL user, log in and create the private MPI DB Link:
+ 
+ CREATE DATABASE LINK "MPI"
+ CONNECT TO "PATIENT" IDENTIFIED BY <password>
+ USING 'hssc-cdw-mpidb-d:1521/mpidev'
+ ;
+ 
+ TODO: Orchestrate creation of user with grants and creation of private DB
+       link, which requires directly connecting as the new user.
+ */
+create user HSSC_ETL identified by changeme;
+
+grant CREATE SESSION to HSSC_ETL;
+grant SELECT ANY TABLE to HSSC_ETL;
+grant CREATE DATABASE LINK to HSSC_ETL;
+
+BEGIN
+  FOR x IN (SELECT * FROM dba_tables WHERE owner in ('CDW'))
+  LOOP
+    EXECUTE IMMEDIATE 'GRANT SELECT, INSERT, UPDATE, DELETE ON '
+                       || x.owner || '.' || x.table_name
+                       || ' TO HSSC_ETL';
+  END LOOP;
+END;
